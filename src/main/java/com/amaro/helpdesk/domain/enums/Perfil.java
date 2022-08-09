7c173f21d7c799886a6d2a31e86b50ab9c204b1d
amarojc <inf.amaro.jc@gmail.com>
2022-08-08 23:09:31 -0300

    tipos enumeradados criados

---
+++ src/main/java/com/amaro/helpdesk/domain/enums/Perfil.java
@@ -0,0 +1,53 @@
+package com.amaro.helpdesk.domain.enums;
+
+public enum Perfil {
+	//Importante número para não gerar problemas futuros, caso um novo
+	// desenvolvedor for fazer manutenção no projeto e acrescentar mais perfis 
+	
+	ADM(0, "ROLE_ADMIN"), 
+	CLIENTE(1,"ROLE_CLIENTE"), 
+	TECNICO(2,"ROLE_TECNICO");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	private Perfil() {
+	}
+
+	private Perfil(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	//Static para não precisar instanciar o método em outra parte do projeto.
+	public static Perfil toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		//Para cada perfil...
+		for(Perfil x : Perfil.values()) {
+			if(cod.equals(x.getCodigo())) {
+				return x;
+			}
+				
+		}
+		throw new IllegalArgumentException("Perfil inválido");
+	}
+}
---
+++ src/main/java/com/amaro/helpdesk/domain/enums/Status.java
@@ -0,0 +1,48 @@
+package com.amaro.helpdesk.domain.enums;
+
+public enum Status {
+	ADM(0, "ABERTO"), 
+	CLIENTE(1,"ANDAMENTO"), 
+	TECNICO(2,"ENCERRADO");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	private Status() {
+	}
+
+	private Status(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public static Status toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(Status x : Status.values()) {
+			if(cod.equals(x.getCodigo())) {
+				return x;
+			}
+				
+		}
+		throw new IllegalArgumentException("Status inválido");
+	}
+}
