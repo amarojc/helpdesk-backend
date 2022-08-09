027eb6eb7c75e45425d47cb649dd57c36109f526
amarojc <inf.amaro.jc@gmail.com>
2022-08-08 23:50:14 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/amaro/helpdesk/domain/Pessoa.java
@@ -0,0 +1,123 @@
+package com.amaro.helpdesk.domain;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import com.amaro.helpdesk.domain.enums.Perfil;
+
+public abstract class Pessoa {
+	
+	protected  Integer id;
+	protected String nome;
+	protected String cpf;
+	protected String email;
+	protected String senha;
+	
+	//Set não permite que tenha dois valores iguais dentro da lista.
+	//New para inicializar o Objeto e não ocorrer um null pointer (Ponteiro nulo).
+	protected Set<Integer> perfis =  new HashSet<>();
+	
+	protected LocalDate dataCriacao = LocalDate.now();
+	
+	public Pessoa() {
+		super();
+		//Considerar que toda pessoa criada sem passar nenhum parâmetro
+		// ela terá um perfil de cliente.
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Set<Perfil> getPerfis() {
+		//Mapear a lista (x) chamar o Perfil, verificando cada perfil com o método toEnum criado.
+		//Coleto tudo e após faço a conversão para o tipo set.
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+
+	//Adicionando apenas o código do perfil, na lista.
+	public void addPerfil(Perfil perfil) {
+		this.perfis.add(perfil.getCodigo());
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
+	}
+	
+	
+	
+
+}
