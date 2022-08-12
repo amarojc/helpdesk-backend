326c7939e3fc84c2e8a80c26cbff11a2cc2f0b9e
amarojc <inf.amaro.jc@gmail.com>
2022-08-11 21:55:32 -0300

    exceção para CPF e EMAIL já cadastrados

--- src/main/java/com/amaro/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/amaro/helpdesk/repositories/PessoaRepository.java
@@ -1,5 +1,7 @@
 package com.amaro.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.amaro.helpdesk.domain.Pessoa;
@@ -7,4 +9,6 @@ import com.amaro.helpdesk.domain.Pessoa;
 //Passagem uma classe e um tipo primitivo do objeto identificador da nossa classe....
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
 
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
--- src/main/java/com/amaro/helpdesk/services/TecnicoService.java
+++ src/main/java/com/amaro/helpdesk/services/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.amaro.helpdesk.domain.Pessoa;
 import com.amaro.helpdesk.domain.Tecnico;
 import com.amaro.helpdesk.domain.dtos.TecnicoDTO;
+import com.amaro.helpdesk.repositories.PessoaRepository;
 import com.amaro.helpdesk.repositories.TecnicoRepository;
+import com.amaro.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.amaro.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,6 +19,9 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	public Tecnico findById(Integer id) {
 		//Pode encontrar ou não o objeto no banco.
@@ -35,8 +41,31 @@ public class TecnicoService {
 		//Id null por questões de segurança, caso seja passado algum valor para o id, na requisição.
 		//Para o BD não entender que seja um update.
 		objDTO.setId(null);
+		
+		//Método para verificar se CPF já está cadastrado no banco
+		validaPorCpfEEmail(objDTO);
+		
 		Tecnico newObj = new Tecnico(objDTO);
 		//Chamada assincrona -> Primeiro irá salvar no banco e depois retonar o insert com o id do Tecnico criado.
 		return repository.save(newObj);
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		
+		/* Se o objeto instanciado for presente, significa que ele existe.
+		   E se obj.get.getId for diferente de objDTO.getId
+		   Lança uma exceção... 
+		 */
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema! " + objDTO.getCpf());
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
+			throw new DataIntegrityViolationException("Email já cadastrado no sistema! " + objDTO.getEmail());
+		}
+		
 	}
 }
